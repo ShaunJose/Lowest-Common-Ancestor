6a54b07a91f311a6ffd6594f9655368c89ce2b47
ShaunJose <shaun/jose12@gmail.com>
2018-09-22 18:39:29 +0100

    Node class creation

--- Lowest_Common_Ancestor.py
+++ Lowest_Common_Ancestor.py
@@ -1,6 +1,19 @@
+# author : Shaun Jose
+# Date created: 22/09/2018
+
+# Node class
+# Attributes: left, right, data
+# NOTE: data can be int, float, double, or char type
+
+class Node:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
 
 # Lowest Common Ancestor class
 # Finds the LCA of two nodes in a Binary Tree
 
-#BinaryTree class (ordered Binary Tree i.e. Binary Search Tree)
-#methods: isEmpty(self), size(self), add(self, val), delete(?), LCA(self, Node, Node)
+# BinaryTree class (ordered Binary Tree i.e. Binary Search Tree)
+# methods: isEmpty(self), size(self), add(self, val), delete(?), LCA(self, Node, Node)
