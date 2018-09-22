f3191b43291d71dccb5843e583f2ab5afbd1d080
ShaunJose <shaun/jose12@gmail.com>
2018-09-22 18:45:00 +0100

    Removing Unnecessary methods like delete

--- Lowest_Common_Ancestor.py
+++ Lowest_Common_Ancestor.py
@@ -16,7 +16,7 @@ class Node:
 # Finds the LCA of two nodes in a Binary Tree
 
 # BinaryTree class (ordered Binary Tree i.e. Binary Search Tree)
-# Methods: isEmpty(self), size(self), add(self, val), delete(?), LCA(self, Node, Node)
+# Methods: isEmpty(self), add(self, val), LCA(self, Node, Node)
 # Attributes: root, size
 
 class BinaryTree:
@@ -26,21 +26,13 @@ class BinaryTree:
         self.root = None
         self.size = 0
 
+
     # returns True if Tree is Empty, False otherwise
     def isEmpty(self):
 
 
-    # returns the number of nodes in the binary tree
-    def size(self):
-
-
     # adds a node in the Binary Tree
     def add(self, val):
-
-
-    # deletes a node from a binary search tree
-    # TODO: Check if needed
-    def delete(self):
 
 
     # finds the lowest common ancestor of two nodes, (inclusive of the nodes themselves)
