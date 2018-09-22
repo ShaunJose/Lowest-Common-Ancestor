be1dea75f5ba30d0f2bcc894529544705b629a6d
ShaunJose <shaun/jose12@gmail.com>
2018-09-22 18:42:42 +0100

    Incomplete API of Binary Tree

--- Lowest_Common_Ancestor.py
+++ Lowest_Common_Ancestor.py
@@ -16,4 +16,32 @@ class Node:
 # Finds the LCA of two nodes in a Binary Tree
 
 # BinaryTree class (ordered Binary Tree i.e. Binary Search Tree)
-# methods: isEmpty(self), size(self), add(self, val), delete(?), LCA(self, Node, Node)
+# Methods: isEmpty(self), size(self), add(self, val), delete(?), LCA(self, Node, Node)
+# Attributes: root, size
+
+class BinaryTree:
+
+    # Initializes an Empty Binary Tree
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    # returns True if Tree is Empty, False otherwise
+    def isEmpty(self):
+
+
+    # returns the number of nodes in the binary tree
+    def size(self):
+
+
+    # adds a node in the Binary Tree
+    def add(self, val):
+
+
+    # deletes a node from a binary search tree
+    # TODO: Check if needed
+    def delete(self):
+
+
+    # finds the lowest common ancestor of two nodes, (inclusive of the nodes themselves)
+    def LCA(self, node_1, node_2):
