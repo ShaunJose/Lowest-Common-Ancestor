83e83e493baeb800b1ecd20b4ead18d3baf92dbd
ShaunJose <shaun/jose12@gmail.com>
2018-09-23 14:56:11 +0100

    Test class API created

--- LCA_Tester.py
+++ LCA_Tester.py
@@ -1,1 +1,35 @@
 # Tester of the BinaryTree class, (focus on LCA implementation)
+
+# Pytest framework used
+
+# Test outcome of LCA for an empty binary tree
+def test_empty_tree():
+
+
+# Test outcome of LCA for null node/s
+def test_null_nodes():
+    #pass both node1 == node2 == null
+
+    #pass node2 == null
+
+    #pass get(val) such that val not in tree
+
+
+# LCA Test with node1 == node2 (testing same node)
+def test_same_nodes():
+
+
+# LCA Test with only left children
+def test_left_tree():
+
+
+# LCA Test with only right children
+def test_right_tree():
+
+
+# Test LCA for 2 nodes in sufficiently populated binary tree
+def test_populated_tree():
+
+
+# Test case where node1 and node2 are in different trees
+def test_different_tree_nodes():
