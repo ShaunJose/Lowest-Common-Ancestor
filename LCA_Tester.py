6c746fdfa9197e7fa6ea6d853906661c300d4f2a
ShaunJose <shaun/jose12@gmail.com>
2018-09-25 17:51:43 +0100

    Empty Tree and Null Nodes Tests

--- LCA_Tester.py
+++ LCA_Tester.py
@@ -2,9 +2,13 @@
 
 # Pytest framework used
 
+from Lowest_Common_Ancestor import BinaryTree, Node
+
 # Test outcome of LCA for an empty binary tree with None(null) Nodes
 def test_empty_tree():
+    bt = BinaryTree()
 
+    assert (bt.LCA(None, None) == None)
 
 # Test outcome of LCA for an empty tree with nodes that exist
 #def test_empty_tree_with_existing_nodes():
@@ -13,15 +17,30 @@ def test_empty_tree():
 
 # Test outcome of LCA for a populated binary tree with None(null) node/s passed
 def test_None_nodes():
-    #pass both node1 == node2 == None
+    #Creating binary tree
+    bt = BinaryTree()
+    bt.add(7) #non-empty binary tree
+    bt.add(12)
+    bt.add(232)
+    bt.add(8)
+    bt.add(2)
 
-    #pass node2 == None
+    #passing both node1 == node2 == None
+    assert (bt.LCA(None, None) == None)
 
-    #pass get(val) such that val not in tree
+    #passing node2 == None
+    assert (bt.LCA(bt.get(7), None) == None)
+
+    #passing get(val) such that val not in tree
+    #neither 7 nor 13 exists in tree
+    assert (bt.LCA(bt.get(7), bt.get(13)) == None)
 
 
 # LCA Test with node1 == node2 (testing same node)
 def test_same_nodes():
+    #with one node bt
+
+    #with sufficiently populated bt
 
 
 # LCA Test with only left children
